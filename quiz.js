ca1d095f643d39857630e75f542fad3993d2d4e2
MennaMedhatt <menna.medhat@thefabulous.co>
2025-01-19 15:02:26 +0200

    format

--- quiz.js
+++ quiz.js
@@ -1,14 +1,14 @@
 function checkAnswer() {
     const correctAnswer = "4";
-    const userAnswer =  document.querySelector('input[name="quiz"]:checked').value;
-    
-    if(userAnswer === correctAnswer){
-    const feedback = document.getElementById("feedback");
-    feedback.innerHTML = "Correct! Well done.";
-}else {
-    const feedback = document.getElementById("feedback");
-    feedback.innerHTML = "That's incorrect. Try again!";
-}
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+
+    if (userAnswer === correctAnswer) {
+        const feedback = document.getElementById("feedback");
+        feedback.innerHTML = "Correct! Well done.";
+    } else {
+        const feedback = document.getElementById("feedback");
+        feedback.innerHTML = "That's incorrect. Try again!";
+    }
 }
 const submitButton = document.getElementById("submit-answer");
 submitButton.addEventListener("click", checkAnswer);
